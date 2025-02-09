b57666ead2f62f787f027a28a18fe58b7876b8b2
jisan <mhhaque4@gmail.com>
2025-02-09 15:31:42 +0600

    navbar added

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { Input } from "@/components/ui/input";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { ChevronDown, Search } from "lucide-react";
+import { tree } from "next/dist/build/templates/app-page";
+import Image from "next/image";
+import React from "react";
+
+const navItems = [
+  { label: "Study Abroad", hasDropdown: true },
+  { label: "Student Service", hasDropdown: true },
+  { label: "About Us ", hasDropdown: false },
+  { label: "Contacts", hasDropdown: false },
+];
+
+export default function Navbar() {
+  return (
+    <div>
+      <header>
+        <div className="container mx-auto flex items-center justify-between h-full">
+          <div className="flex items-center gap-3">
+            <Image src="" alt="" width={60} height={60} className="" />
+            <span className="font-bold text-[24.7px] tracking-[1.65px] text-[#1a1a1a]">
+              BRIGHTMINDS
+            </span>
+          </div>
+          <div className="flex-1 max-w-[409px]  mx-auto">
+            <div className="flex items-center gap-4 p-4 bg-[#f7931e0a] rounded-[18px] border border-[#f7931e]">
+              <Search className="w-[18px] h-[18px] text-[#6f6f6f]" />
+              <Input
+                className="border-none bg-transparent placeholder:text-[#6f6f6f]"
+                placeholder="Find courses, subjects, universities...."
+              />
+            </div>
+          </div>
+
+          <NavigationMenu>
+            <NavigationMenuList>
+              {navItems.map((item) => (
+                <NavigationMenuItem key={item.label}>
+                  <NavigationMenuLink className="flex items-center gap-1 px-2 py-1">
+                    {item.label}
+                    {item.hasDropdown && <ChevronDown className="w-4 h-4" />}
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </header>
+    </div>
+  );
+}
