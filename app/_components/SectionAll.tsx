a9895802da48182aa9ed775d6aca84cd33294529
jisan <mhhaque4@gmail.com>
2025-02-10 21:06:01 +0600

    sectionAll done

--- app/_components/SectionAll.tsx
+++ app/_components/SectionAll.tsx
@@ -1,19 +1,39 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { Heart, Send } from "lucide-react";
+import { Heart, Home, Send } from "lucide-react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSackDollar } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const feature = [
   {
-    icon: <Heart className="w-10 h-10" />,
+    icon: <Heart className="w-10 h-10 text-orange-400" />,
     title: "Easy apply",
     description:
       "Filter, shortlist, apply and pay to study your dream course with ease.",
   },
   {
-    icon: <Send className="w-10 h-10" />,
+    icon: (
+      <FontAwesomeIcon
+        icon={faSackDollar}
+        className="w-10 h-10 text-orange-400"
+      />
+    ),
+    title: "Education loan",
+    description:
+      "Get pre-approved loans in 48 hours, expert consultation, any apply onlie with Brightminds Funds. ",
+  },
+  {
+    icon: <Send className="w-10 h-10 text-orange-400" />,
     title: "Visa Application",
     description:
       "Get end-to-end guidance on your visa application from an expert.",
+  },
+  {
+    icon: <Home className="w-10 h-10 text-orange-400" />,
+    title: "Accomodation help",
+    description:
+      "Find budget-friendly rooms near 700+ global cities, and get expert guidance with Brightminds Stays.",
   },
 ];
 
