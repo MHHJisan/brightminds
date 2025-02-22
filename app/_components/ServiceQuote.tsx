15e2bcf49d5b25be643d925a8b6d8435651e1ae0
jisan <mhhaque4@gmail.com>
2025-02-22 16:15:30 +0600

    quote image added

--- app/_components/ServiceQuote.tsx
+++ app/_components/ServiceQuote.tsx
@@ -1,5 +1,7 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Card, CardContent } from "@/components/ui/card";
-import { GraduationCap } from "lucide-react";
+import { QuoteIcon } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 export default function ServiceQuote() {
@@ -7,7 +9,20 @@ export default function ServiceQuote() {
     <div className="w-full max-w-[1154px] mx-auto mb-12">
       <Card className="relative bg-[#fffcfa] border-[#f68e13] shadow-[0px_4px_15.7px_1px_#0000000f]">
         <CardContent className="flex flex-col items-center py-8 px-16">
-          <GraduationCap className="w-[49px] h-[49px] text-[#f68e13] mb-4" />
+          {/* <QuoteIcon className="w-[49px] h-[49px] text-[#f68e13] mb-4" /> */}
+          <Card className="w-[49px] h-[49px] relative bg-transparent border-none shadow-none mb-4">
+            <CardContent className="p-0">
+              <AspectRatio ratio={1}>
+                <Image
+                  src="/quote.png"
+                  alt="Quote"
+                  loading="lazy"
+                  width={50}
+                  height={50}
+                />
+              </AspectRatio>
+            </CardContent>
+          </Card>
           <p className="text-center text-[22px] leading-[37.6px] text-[#535353] font-semibold [font-family: 'Inter-Semibold', Helvetica] max-w-[1019px]">
             Brightminds Is Dedicated To Helping Internatinal Students
             Successfully Navigate The U.S University Admissions Process. With
