1254d25f625fe75ec2d39fe4e37ebc448bbd6d6b
jisan <mhhaque4@gmail.com>
2025-02-24 21:31:06 +0600

    added paragraph in about

---
+++ app/about/_components/Paragraph.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Paragraph() {
+  return (
+    <div className="px-40 my-8">
+      <h1 className="[font-family: 'Inter-Bold', Helvetica] font-bold text-[#1a1a1a] text-[40px] leading-[50px] tracking-[0]">
+        The Inception Of BrightMinds
+      </h1>
+      <p className="[font-family: 'Inter-SemiBold', Helvetica] font-semibold text-[#535353] text-[22px] leading-[37.6px] py-8">
+        <span>
+          Brightminds Was Founded With a clear vision-to empower International
+          Students with the righ guidance to Navigate the comples U.S admissions
+          preocess. Recognising the challenges students face, from understanding
+          university requirements to craft compelling Applications, we set out
+          to bridge the gap with expert support and Personalized strategies.
+        </span>
+        <span>
+          Our Journey began with a passion for education and a commitment to
+          making U.S higher education accessible to aspiring students worldwide.
+          Since our inception we have helped countless students achieve their
+          academic dreams, ensuring they find the right university fit while
+          Simplifying every step of the Admissions Journey
+        </span>
+        <span>
+          At Brightminds, We believe that every student deserve the opportunity
+          to succeed, and we are here to make that happen.
+        </span>
+      </p>
+    </div>
+  );
+}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,12 +4,14 @@ import Footer from "../_components/Footer";
 import SectionSocialData from "../_components/SectionSocialData";
 import Subscribe from "../_components/Subscribe";
 import Header from "./_components/Header";
+import Paragraph from "./_components/Paragraph";
 
 export default function About() {
   return (
     <div>
       <Navbar />
       <Header />
+      <Paragraph />
       <Subscribe />
       <SectionSocialData />
       <Footer />
